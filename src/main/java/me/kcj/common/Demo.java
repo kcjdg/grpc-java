714bde27fca235250b3a47858eba116b44a260e2
kcjdg <kencrisjohn@gmail.com>
2024-07-27 22:34:15 +0800

    Flow control

--- src/main/java/me/kcj/common/Demo.java
+++ src/main/java/me/kcj/common/Demo.java
@@ -2,10 +2,11 @@ package me.kcj.common;
 
 import me.kcj.sec06.BankService;
 import me.kcj.sec06.TransferService;
+import me.kcj.sec07.FlowControlService;
 
 public class Demo {
     public static void main(String[] args) {
-        GrpcServer.create(new BankService(), new TransferService())
+        GrpcServer.create(new FlowControlService())
                 .start()
                 .await();
     }
